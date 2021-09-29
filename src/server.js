0524fca5542ebc098db77323da427dc7faee37c7
PLECON <50894668+gchlee2@users.noreply.github.com>
2021-09-29 16:34:12 +0900

    ws 기본기능을 이용한 채팅 구현

--- src/server.js
+++ src/server.js
@@ -15,13 +15,27 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser 😀");
     socket.on("close", () => console.log("Disconnected from the Browser 😪"));
-    socket.on("message", (message) => {
-        console.log(message.toString('utf8'));
-    });
-    socket.send("hello!!!");
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+          case "new_message":
+            sockets.forEach((aSocket) =>
+              aSocket.send(`${socket.nickname}: ${message.payload}`)
+            );
+            break;
+          case "nickname":
+            console.log("닉네임 변경");
+            socket["nickname"] = message.payload;
+            break;
+        }
+      });
 });
 
 server.listen(3000, handleListen);
